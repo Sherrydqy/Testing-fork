c067aa2defd019eea99874399d5050ec9bc768ec
Qinyu <1009908794@qq.com>
2020-04-27 12:15:06 -0400

    added tester2

--- Testing-fork/ViewController.swift
+++ Testing-fork/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        print("ss");
         // Do any additional setup after loading the view.
     }
 
